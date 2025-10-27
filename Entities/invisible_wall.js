a8fffa183b92e430de00cf05409856abf519ca79
Pechersky Efim <pecherskii0404@gmail.com>
2025-10-27 14:57:46 +0800

    Добавил мост во второй сцене

--- Entities/invisible_wall.js
+++ Entities/invisible_wall.js
@@ -1,14 +1,18 @@
 export class Wall {
-    constructor(x, z, y, w, h, angle) {//x,y,z позиция 
+    constructor(x, z, y, w, h, angle, collide=true,d=5) {//x,y,z позиция 
         this.entity = document.createElement('a-box');
-        this.setupWall(x, z, y, w, h, angle);
+        this.setupWall(x, z, y, w, h,d, angle, collide);
     }
-    setupWall(x, z, y, w, h, angle) {
+    setupWall(x, z, y, w, h,d, angle, collide) {
         this.entity.setAttribute('position', `${x} ${y} ${z}`);
-        this.entity.setAttribute('class', "collision wall")
+        if (collide){
+            this.entity.setAttribute('class', "collision wall")
+        }else{
+            this.entity.setAttribute('class', "collision")
+        }
         this.entity.setAttribute('width', `${w}`)
         this.entity.setAttribute('height', `${h}`)
-        this.entity.setAttribute('depth', "5")
+        this.entity.setAttribute('depth', `${d}`)
         this.entity.setAttribute('color', "red")
         this.entity.setAttribute('rotation', `0 ${angle} 0`)
         this.entity.setAttribute('visible', false)
--- Scenes/second_cave.js
+++ Scenes/second_cave.js
@@ -3,6 +3,7 @@ import { Ball } from '../Entities/ball.js';
 import { Cave } from '../Entities/cave.js';
 import { Wall } from '../Entities/invisible_wall.js';
 import { Light } from '../Entities/light.js';
+import {Bridge} from '../Entities/bridge.js';
 import { Floor } from '../Entities/floor.js';
 import { BallDestroyer } from '../Entities/ball_destroyer.js';
 import { Exit } from '../Entities/exit.js';
@@ -20,7 +21,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const backwall2 = new Wall(0, 100, 0, 100, 1000, 0);
     const downwall1 = new Wall(0, 62, -32, 120, 50, 0);
     const downwall2 = new Wall(0, 11, -30, 120, 50, 0);
-    const ball_destroyer = new BallDestroyer(0, -40, 30, 150, 50, 50)
+    const barier1 = new Wall(50, 36, 0, 25, 120, 0, false, 48);
+    const barier2 = new Wall(0, 36, 0, 25, 120, 0, false, 48);
+    const barier3 = new Wall(25, 36, 0, 25, 120, 0, false, 48);
+    const bridge = new Bridge(25, 60, 20,10,60)
+    const ball_destroyer = new BallDestroyer(0, -40, 30, 150, 60, 60)
     const exit = new Exit(20, 0, 90, 50, 20, 20, "http://127.0.0.1:5500/third_scene.html");
     scene.appendChild(exit.getEntity())
     scene.appendChild(ball_destroyer.getEntity())
@@ -30,20 +35,36 @@ document.addEventListener('DOMContentLoaded', function () {
     scene.appendChild(backwall2.getEntity());
     scene.appendChild(downwall1.getEntity());
     scene.appendChild(downwall2.getEntity());
+    scene.appendChild(barier1.getEntity());
+    scene.appendChild(barier2.getEntity());
+    scene.appendChild(barier3.getEntity());
     const floor1 = new Floor(0, -6, -2, 0, 120, 30)
     const floor2 = new Floor(0, 0, 90, -15, 120, 60)
+    const floor3 = new Floor(25, -8, 40, 0, 20, 60)
     scene.appendChild(floor1.getEntity());
     scene.appendChild(floor2.getEntity());
     var x_limit = [-20, 55]
     var z_limit = [-7, 11]
     const ball = new Ball(0, -1.5, 0, 0.7, -6);
     scene.appendChild(ball.getEntity());
+    scene.appendChild(bridge.getEntity())
     console.log(exit.getEntity().getAttribute('position'))
     console.log(player.getEntity().getAttribute('position'))
+    var start=false;
+    var floor_added=false
     function gameLoop() {// это для того чтобы мяч двигался с игроком
 
         if (ball.isPicked) {
             exit.isOpen = true;
+            start=true;
+        }
+        if (ball.checkSphereBoxCollision(ball.entity,bridge.entity) && start && !floor_added){
+            ball.pickUp()
+            floor_added=true
+            scene.appendChild(floor3.getEntity());
+            scene.appendChild(barier3.getEntity());
+            ball.allFloor=ball.findAllFloor()
+            bridge.raise()
         }
         ball.update();
         requestAnimationFrame(gameLoop);
